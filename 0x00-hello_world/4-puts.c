d548537dced16aebfe0271dbe8d3399127bd1a0e
Philopatear <philopatearabdelfady96@gmail.com>
2023-02-17 16:02:05 -0800

    Making 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    my_string[] = "\"Programming is like building a multilingual puzzle";
+    puts(my_string);
+    return 0;
+}
