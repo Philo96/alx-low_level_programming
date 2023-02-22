5e5ef1e4add5885a9d42e4ff1c317041878accd9
Philo96 <philopatearabdelfady96@gmail.com>
2023-02-22 20:02:14 +0200

    Editting 2-print_alphabet_x10 v0001

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 char letter = 'a';
 int num = 0;
 
-for (num; num < 10; num = num + 1)
+for (; num < 10; num = num + 1)
 {
-for (letter; letter <= 'z'; letter++)
+for (; letter <= 'z'; letter++)
 {
 _putchar(letter);
 }
