4dfe8b86656251de3ee02174492aaa2c50a1e24a
Philo96 <philopatearabdelfady96@gmail.com>
2023-02-22 19:40:52 +0200

    Editting 1-alphaber.c v0003

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,13 @@
 
 /**
  * main - check the code
- * 
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
 char letter = 'a';
 
-while(letter <= 'z')
+while (letter <= 'z')
 {
 _putchar(letter);
 letter++;
