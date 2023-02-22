8ee12a467072b7a95b4a39456e2c05fb15f727fd
Philo96 <philopatearabdelfady96@gmail.com>
2023-02-22 19:58:55 +0200

    Making 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphapets ten times.
+ * Return: 0.
+ */
+void print_alphabet_x10(void)
+{
+char letter = 'a';
+int num = 0;
+
+for (num; num < 10; num = num + 1)
+{
+for (letter; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
+}
