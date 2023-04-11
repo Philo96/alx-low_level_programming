6d3d4b1fcc4c92daa135a5c27424cab28f55f3f8
Philopatear Abdelmaseh <94920732+Philo96@users.noreply.github.com>
2023-04-11 03:23:38 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,20 @@
-#include "main.h"
-
-/**
- * print_binary - Prints the binary representation of a number
- * @n: Number to print in binary
- */
-void print_binary(unsigned long int n)
+unsigned int binary_to_uint(const char *b)
 {
-    unsigned long int mask = 1;
-    int bit_pos = 0;
+	unsigned int num = 0;
 
-    /* Find the position of the most significant bit */
-    while ((n >> bit_pos) > 0)
-        bit_pos++;
+	if (b == NULL)
+		return (0);
 
-    /* Print the bits in reverse order */
-    while (bit_pos--)
-    {
-        putchar(((n & (mask << bit_pos)) > 0) + '0');
-    }
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		num = num << 1;
+		if (*b == '1')
+			num += 1;
+		b++;
+	}
+
+	return (num);
 }
