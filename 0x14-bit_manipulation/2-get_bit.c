bdd2fe262ea73388721fa7e6728363b5b8f99bb8
Philo96 <philopatearabdelfady96@gmail.com>
2023-07-07 02:29:55 +0300

    reformate the code in '2-get_bit.c' file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,14 +9,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned long int div, res;
+unsigned long int div, res;
 
-    if (index > (sizeof(unsigned long int) * 8 - 1))
-        return (-1);
-    div = 1 << index;
-    res = n & div;
-    if (res == div)
-        return (1);
+if (index > (sizeof(unsigned long int) * 8 - 1))
+return (-1);
+div = 1 << index;
+res = n & div;
+if (res == div)
+return (1);
 
-    return (0);
+return (0);
 }
