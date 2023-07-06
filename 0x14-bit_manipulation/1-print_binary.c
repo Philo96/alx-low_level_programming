fe59cd044c10a18d2ca65f509ba3a66e7868c2ba
Philo96 <philopatearabdelfady96@gmail.com>
2023-07-07 02:32:19 +0300

    reformate the code in '1-print_binary.c'

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,13 +7,13 @@
  */
 unsigned long int _power(unsigned int base, unsigned int pow)
 {
-    unsigned long int num;
-    unsigned int i;
+unsigned long int num;
+unsigned int i;
 
-    num = 1;
-    for (i = 1; i <= pow; i++)
-        num *= base;
-    return (num);
+num = 1;
+for (i = 1; i <= pow; i++)
+num *= base;
+return (num);
 }
 /**
  * print_binary - prints the binary representation of a number
@@ -22,25 +22,25 @@ unsigned long int _power(unsigned int base, unsigned int pow)
  */
 void print_binary(unsigned long int n)
 {
-    unsigned long int dev, result;
-    char flag;
+unsigned long int dev, result;
+char flag;
 
-    flag = 0;
-    dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+flag = 0;
+dev = _power(2, sizeof(unsigned long int) * 8 - 1);
 
-    while (dev != 0)
-    {
-        result = n & dev;
-        if (result == dev)
-        {
-            flag = 1;
-            _putchar('1');
+while (dev != 0)
+{
+result = n & dev;
+if (result == dev)
+{
+flag = 1;
+_putchar('1');
 
-        }
-        else if (flag == 1 || dev == 1)
-        {
-            _putchar('0');
-        }
-        dev >>= 1;
-    }
+}
+else if (flag == 1 || dev == 1)
+{
+_putchar('0');
+}
+dev >>= 1;
+}
 }
